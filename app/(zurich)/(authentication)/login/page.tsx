c111b90ced0f167fcb8d92f838f62051d8c9aabe
Arvend <arvendrajan100@gmail.com>
2025-04-19 00:43:33 +0800

    initial commit

---
+++ app/(zurich)/(authentication)/login/page.tsx
@@ -0,0 +1,13 @@
+export default function Page() {
+  return (
+    <div className="login">
+      <div className="container">
+        <div className="login__wrapper">
+          {/* Image */}
+          <div className="login__image"></div>
+        </div>
+        <h1>Login</h1>
+      </div>
+    </div>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6">
+            <h1>home page</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
